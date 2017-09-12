fe601b9b5ee468e2b84dc629dc3bd8465794a2bb
Stefan Petrov <stefanp227@gmail.com>
2017-09-12 15:21:21 +0300

    Add another task

---
+++ 2017_redoven_10_potok_grupa_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b) {
+    if (a == 0)
+        return b;
+    return gcd(b%a, a);
+}
+ 
+int findGCD(int arr[], int n) {
+    int result = arr[0];
+    int i;
+
+    for (i=0 ; i <= n; i++)
+        result = gcd(arr[i], result);
+ 
+    return result;
+}
+ 
+int main() {
+    int arr[] = {2, 4, 6, 8, 16};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("%d", findGCD(arr, n));
+    return 0;
+}
